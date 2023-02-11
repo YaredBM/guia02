5cd6fb4fd6876b8684467dd8a77e240a091111cd
Yáred Iessé Bustillo Medina <92419630+YaredBM@users.noreply.github.com>
2023-02-11 16:33:41 -0600

    Update 58_Yared_Bustillo_Ejercicio_04.py

--- 58_Yared_Bustillo_Ejercicio_04.py
+++ 58_Yared_Bustillo_Ejercicio_04.py
@@ -1,4 +1,5 @@
 #EJERCICIO 04
+#Un programa que pida introducir una cadena y contar cuantas palabras hay en ella.
 frase = str(input("Ingrese la frase: "))
 print ("La frase original es : " + frase)
 resultado = frase.count(" ")+1
