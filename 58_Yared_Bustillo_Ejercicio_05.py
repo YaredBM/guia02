608e9f005c043d83b98478fe56a78b942bb16a0d
Yáred Iessé Bustillo Medina <92419630+YaredBM@users.noreply.github.com>
2023-02-11 16:34:07 -0600

    Update 58_Yared_Bustillo_Ejercicio_05.py

--- 58_Yared_Bustillo_Ejercicio_05.py
+++ 58_Yared_Bustillo_Ejercicio_05.py
@@ -1,4 +1,5 @@
 #EJERCICIO 05
+#Un programa que pida introducir una cadena de caracteres y la imprima sin espacios.
 frase = str(input("Ingresa una frase: "))
 noespacios = frase.replace(" ", "")
 print(noespacios)
