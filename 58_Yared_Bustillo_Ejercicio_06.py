d30328dd11276b5c361927717b2b13164d4a99cd
Yáred Iessé Bustillo Medina <92419630+YaredBM@users.noreply.github.com>
2023-02-11 16:34:41 -0600

    Update 58_Yared_Bustillo_Ejercicio_06.py

--- 58_Yared_Bustillo_Ejercicio_06.py
+++ 58_Yared_Bustillo_Ejercicio_06.py
@@ -1,4 +1,6 @@
 #EJERCICIO 06
+#Escriba un programa que lea una frase y a continuación visualice cada palabra 
+#de la frase en columnas, seguida del número de letras que tiene cada palabra.
 frase = str(input("Ingresa una frase: "))
 palabras = frase.split()
 contar_letra_palabra = {p:len(p) for p in palabras}
